1ea9b9ef8441c99dc6ef65452a5c54fa18552fe8
Adriana McGee <adriana@Adrianas-MBP.lan>
2022-06-08 00:47:14 -0400

    commit tree 51f1d6ed67797313b50673f5abf45c60d41ff198

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    return [...cats, name];
+}
+
+function prependCat(name){
+    return [name, ...cats];
+}
+
+function removeLastCat(){
+    cats.slice(-1);
+    return cats;
+}
